b954324ba76904ab054da79d064d1b1e766b70f1
= <melissa.iman.n@uni.pe>
2024-05-17 11:25:17 -0500

    landing page

--- red_social_web/src/App.jsx
+++ red_social_web/src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css"
 import { BrowserRouter as   Router, Route, Routes } from "react-router-dom"
-
+import * as React from "react"
 import { routes } from "./utils/routes"
+import Home from "./pages/home/Home"
+import Navbar from "./pages/home/components/Navbar"
+import HomePage from "./pages/home/components/HomePage"
 
 function App() {
   return (
+    <>
+      <Navbar/>
+      <HomePage/>
+    </>
+    
+    /*
     <Router>
-      {/* <Navbar/> */}
+      
       <Routes>
         {routes.map((r) => {
           return <Route path={r.path} element={r.element} key={r.id} />
         })}
       </Routes>
     </Router>
+    */
+    
   )
 }
 export default App
